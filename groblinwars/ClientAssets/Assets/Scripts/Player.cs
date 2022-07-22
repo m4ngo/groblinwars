7f401e156b3c6248908e740b165424ea203ed1c5
arik4231 <arik4231@gmail.com>
2022-07-22 13:03:44 -0700

    adsf

---
+++ groblinwars/ClientAssets/Assets/Scripts/Player.cs
@@ -0,0 +1,149 @@
+using RiptideNetworking;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public static Dictionary<ushort, Player> list = new Dictionary<ushort, Player>();
+
+    public ushort Id { get; private set; }
+    public bool IsLocal { get; private set; }
+
+    [SerializeField] private PlayerAnimationManager animationManager;
+    [SerializeField] private PlayerController controller;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private Interpolator interpolator;
+    [SerializeField] private TextMeshPro nameTag;
+    private CameraController camController;
+
+    [Header("Customize")]
+    [SerializeField] private GameObject[] baseObj;
+    [SerializeField] private GameObject shirtObj;
+    [SerializeField] private GameObject backpackObj;
+    [SerializeField] private Transform hatHolder;
+
+    private string[] colors;
+    private int hat;
+    private string username;
+
+    private void Update()
+    {
+        if (IsLocal)
+            return;
+        if(Camera.main != null)
+            nameTag.transform.LookAt(Camera.main.transform);
+        nameTag.text = list[Id].username;
+    }
+
+    private void OnDestroy()
+    {
+        list.Remove(Id);
+    }
+
+    private void Move(ushort tick, Vector3 newPosition, Vector3 forward, bool isCrouching, bool isCrawling)
+    {
+        interpolator.NewUpdate(tick, newPosition);
+
+        if (!IsLocal)
+        {
+            camTransform.forward = forward;
+            transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+            transform.rotation = Quaternion.Euler(new Vector3(0, transform.eulerAngles.y, 0));
+            animationManager.SetCrouching(isCrouching);
+            animationManager.SetCrawling(isCrawling);
+            animationManager.AnimateBasedOnSpeed();
+        } 
+        else
+        {
+            if(camController == null)
+                camController = camTransform.GetComponent<CameraController>();
+            if (camController != null)
+                camController.Crouch(isCrouching, isCrawling);
+        }
+    }
+
+    private void SetPlayerColors(string[] colors, int hat)
+    {
+        for (int i = 0; i < baseObj.Length; i++)
+        {
+            ColorUtility.TryParseHtmlString(colors[0], out Color baseColor);
+            foreach (GameObject obj in baseObj)
+                obj.GetComponent<MeshRenderer>().material.color = baseColor;
+        }
+
+        ColorUtility.TryParseHtmlString(colors[1], out Color shirtColor);
+        shirtObj.GetComponent<MeshRenderer>().material.color = shirtColor;
+
+        ColorUtility.TryParseHtmlString(colors[1], out Color backpackColor);
+        backpackObj.GetComponent<MeshRenderer>().material.color = backpackColor;
+
+        if(hat != -1)
+            Instantiate(GameLogic.Singleton.Hats[hat], hatHolder.position, hatHolder.rotation).transform.SetParent(hatHolder);
+    }
+
+    public static void Spawn(ushort id, string username, Vector3 position, string[] colors, int hat)
+    {
+        Player player;
+        if (id == NetworkManager.Singleton.Client.Id)
+        {
+            player = Instantiate(GameLogic.Singleton.LocalPlayerPrefab, position, Quaternion.identity).GetComponent<Player>();
+            player.IsLocal = true;
+        } 
+        else
+        {
+            player = Instantiate(GameLogic.Singleton.PlayerPrefab, position, Quaternion.identity).GetComponent<Player>();
+            player.IsLocal = false;
+            player.SetPlayerColors(colors, hat);
+        }
+
+        player.name = $"Player {id} ({(string.IsNullOrEmpty(username) ? "Guest" : username)})";
+        player.Id = id;
+        player.username = username;
+        player.colors = colors;
+
+        list.Add(id, player);
+    }
+
+    public void Die(bool alive)
+    {
+        if (!IsLocal)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(alive);
+            }
+        }
+        else
+            UIManager.Singleton.SetDeathScreen(!alive);
+    }
+
+    #region Messages
+    [MessageHandler((ushort)ServerToClientId.playerSpawned)]
+    private static void SpawnPlayer(Message message)
+    {
+        Spawn(message.GetUShort(), message.GetString(), message.GetVector3(), message.GetStrings(), message.GetInt());
+    }
+
+    [MessageHandler((ushort)ServerToClientId.playerMovement)]
+    private static void PlayerMovement(Message message)
+    {
+        if (list.TryGetValue(message.GetUShort(), out Player player))
+            player.Move(message.GetUShort(), message.GetVector3(), message.GetVector3(), message.GetBool(), message.GetBool());
+    }
+
+    [MessageHandler((ushort)ServerToClientId.grabbedObject)]
+    private static void GrabbedObject(Message message)
+    {
+        if (Player.list.TryGetValue(message.GetUShort(), out Player player))
+            player.controller.UpdateGrabObject(message.GetInt());
+    }
+
+    [MessageHandler((ushort)ServerToClientId.playerDied)]
+    private static void PlayerDied(Message message)
+    {
+        if (Player.list.TryGetValue(message.GetUShort(), out Player player))
+            player.Die(message.GetBool());
+    }
+    #endregion
+}
---
+++ groblinwars/ClientAssets/Assets/Scripts/UIManager.cs
@@ -0,0 +1,179 @@
+using RiptideNetworking;
+using RiptideNetworking.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Net;
+using System.Linq;
+using System;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _singleton;
+    public static UIManager Singleton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != null)
+            {
+                Debug.Log($"{nameof(UIManager)} instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+
+    [Header("Connect")]
+    [SerializeField] private GameObject connectUI;
+    [SerializeField] private InputField usernameField;
+    [SerializeField] private InputField IPField;
+
+    [Space]
+
+    [Header("Customize")]
+    [SerializeField] private GameObject playerModel;
+    [SerializeField] private GameObject[] baseObj;
+    [SerializeField] private GameObject shirtObj;
+    [SerializeField] private GameObject backpackObj;
+
+    [SerializeField] private string[] colors;
+    [SerializeField] private int hat;
+    [SerializeField] private Transform hatHolder;
+    private GameObject currentHat;
+
+    [Space]
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private InputField sensitivityField;
+
+    [Space]
+
+    [Header("Death")]
+    [SerializeField] private GameObject deathScreen;
+
+    private void Awake()
+    {
+        Singleton = this;
+    }
+
+    private void Update()
+    {
+        if (connectUI.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
+        }
+    }
+
+    public void SetDeathScreen(bool active) { deathScreen.SetActive(active); }
+
+    public float GetSensitivity()
+    {
+        return float.Parse(sensitivityField.text);
+    }
+
+    public bool GetPaused()
+    {
+        return pauseMenu.activeInHierarchy;
+    }
+
+    public void ConnectClicked()
+    {
+        if (!IsValidIPAddress(IPField.text))
+            return;
+
+        usernameField.interactable = false;
+        connectUI.SetActive(false);
+        playerModel.SetActive(false);
+
+        NetworkManager.Singleton.Connect(IPField.text);
+    }
+
+    public void DisconnectClicked()
+    {
+        NetworkManager.Singleton.Client.Disconnect();
+        BackToMain();
+        SceneManager.LoadScene(0);
+    }
+
+    public void BackToMain()
+    {
+        usernameField.interactable = true;
+        connectUI.SetActive(true);
+        pauseMenu.SetActive(false);
+        playerModel.SetActive(true);
+    }
+
+    public void SendName()
+    {
+        Message message = Message.Create(MessageSendMode.reliable, ClientToServerId.name);
+        message.AddString(usernameField.text);
+        message.AddStrings(colors);
+        message.AddInt(hat);
+        NetworkManager.Singleton.Client.Send(message);
+    }
+    public bool IsValidIPAddress(string IpAddress)
+    {
+        IPAddress IP;
+        if (IpAddress.Count(c => c == '.') == 3)
+        {
+            bool flag = IPAddress.TryParse(IpAddress, out IP);
+            if (flag)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void SetBaseColor(string color)
+    {
+        ColorUtility.TryParseHtmlString(color, out Color outColor);
+        foreach (GameObject obj in baseObj)
+        {
+            obj.GetComponent<MeshRenderer>().material.color = outColor;
+        }
+        colors[0] = color;
+    }
+    public void SetShirtColor(string color)
+    {
+        ColorUtility.TryParseHtmlString(color, out Color outColor);
+        shirtObj.GetComponent<MeshRenderer>().material.color = outColor;
+        colors[1] = color;
+    }
+    public void SetBackpackColor(string color)
+    {
+        ColorUtility.TryParseHtmlString(color, out Color outColor);
+        backpackObj.GetComponent<MeshRenderer>().material.color = outColor;
+        colors[2] = color;
+    }
+
+    public void SetHat(int index)
+    {
+        hat = index;
+        if(currentHat != null)
+            Destroy(currentHat);
+        if(hat != -1)
+        {
+            currentHat = Instantiate(GameLogic.Singleton.Hats[hat], hatHolder.position, hatHolder.rotation);
+            currentHat.transform.SetParent(hatHolder);
+        }
+    }
+}
